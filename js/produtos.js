5c1c244576c597595282711d1bba4ace3668562c
Ricardo <ricardowestphal@outlook.com>
2021-02-18 18:31:21 -0300

    finalizado sistema de busca simples

--- js/produtos.js
+++ js/produtos.js
@@ -2,28 +2,28 @@ const produtos = [{
         id: 1,
         image: 'https://picsum.photos/200',
         title: 'Produto 01',
-        description: 'Descrição do produto01',
+        description: 'Descrição do produto 01',
         value: 10.00
     },
     {
         id: 2,
         image: 'https://picsum.photos/200',
         title: 'Produto 02',
-        description: 'Descrição do produto02',
+        description: 'Descrição do produto 02',
         value: 20.00
     },
     {
         id: 3,
         image: 'https://picsum.photos/200',
         title: 'Produto 03',
-        description: 'Descrição do produto03',
+        description: 'Descrição do produto 03',
         value: 30.00
     },
     {
         id: 4,
         image: 'https://picsum.photos/200',
         title: 'Produto 04',
-        description: 'Descrição do produto04',
+        description: 'Descrição do produto 04',
         value: 40.00
     },
 ]
