a9f481404600419a21b814be3cc6cf5ed12f44ab
Ricardo <ricardowestphal@outlook.com>
2021-02-19 08:59:05 -0300

    atualizando as fotos dos pratos

--- js/produtos.js
+++ js/produtos.js
@@ -1,27 +1,27 @@
 const produtos = [{
         id: 1,
-        image: 'https://picsum.photos/200',
+        image: './imagens/prato01.jpg',
         title: 'Produto 01',
         description: 'Descrição do produto 01',
         value: 10.00
     },
     {
         id: 2,
-        image: 'https://picsum.photos/200',
+        image: './imagens/prato02.jpg',
         title: 'Produto 02',
         description: 'Descrição do produto 02',
         value: 20.00
     },
     {
         id: 3,
-        image: 'https://picsum.photos/200',
+        image: './imagens/prato03.jpg',
         title: 'Produto 03',
         description: 'Descrição do produto 03',
         value: 30.00
     },
     {
         id: 4,
-        image: 'https://picsum.photos/200',
+        image: './imagens/prato04.jpg',
         title: 'Produto 04',
         description: 'Descrição do produto 04',
         value: 40.00
